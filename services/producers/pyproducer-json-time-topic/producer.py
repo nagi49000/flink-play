a1432091869ca803ff780514f15abfd47fbc4ec0
nagi <nagi>
2022-01-29 17:31:35 +0000

    got python producer speaking to kafka

--- services/producers/pyproducer-json-time-topic/producer.py
+++ services/producers/pyproducer-json-time-topic/producer.py
@@ -9,13 +9,15 @@ import json
 logging.basicConfig(level=logging.DEBUG)
 sleep(10)  # sleep whilst other services come up
 
+# hostname in bootstrap_servers obtained from link in docker-compose.yml
 producer = KafkaProducer(
-    bootstrap_servers=["127.0.0.1:29092"], retries=3,
+    bootstrap_servers=["kafka-flink-play:9092"], retries=3,
     value_serializer=lambda m: json.dumps(m).encode("ascii")
 )
 
 # launch a limited number of messages
-for _ in range(10):
+for _ in range(5):
     producer.send("json-time-topic", {"time": datetime.utcnow().isoformat() + "Z"})
     sleep(3)
 producer.flush()
+producer.close(timeout=5)
