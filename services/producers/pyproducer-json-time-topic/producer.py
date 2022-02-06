f032a949d2276994cee3d6a76c1619d289c84e97
nagi <nagi>
2022-02-06 13:14:54 +0000

    got dockers not crashing

--- services/producers/pyproducer-json-time-topic/producer.py
+++ services/producers/pyproducer-json-time-topic/producer.py
@@ -7,7 +7,7 @@ import json
 
 
 logging.basicConfig(level=logging.INFO)
-sleep(10)  # sleep whilst other services come up
+sleep(5)  # sleep whilst other services come up
 
 # hostname in bootstrap_servers obtained from link in docker-compose.yml
 producer = KafkaProducer(
