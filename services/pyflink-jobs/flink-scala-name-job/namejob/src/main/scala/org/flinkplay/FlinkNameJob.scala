143ac1e36a337aa6e0ef65cb194ea8d554d14e70
nagi <nagi>
2022-04-16 23:44:16 +0100

    got mapwithstate compiling

--- services/pyflink-jobs/flink-scala-name-job/namejob/src/main/scala/org/flinkplay/FlinkNameJob.scala
+++ services/pyflink-jobs/flink-scala-name-job/namejob/src/main/scala/org/flinkplay/FlinkNameJob.scala
@@ -24,15 +24,6 @@ object FlinkNameJob {
       .setValueOnlyDeserializer(new JsonNodeDeserializationSchema())
       .build()
 
-    val dataStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
-      .map(x => x.get("name").asText())
-      .map(x => x.toUpperCase())
-      .map(x => x.split(" ").head)
-      .map(x => Map("firstname" -> x))
-      .map(x => Json(DefaultFormats).write(x))
-
-    dataStream.print()  // debug
-
     val sink = KafkaSink.builder()
       .setBootstrapServers("kafka-flink-play:9092")
       .setRecordSerializer(
@@ -43,6 +34,26 @@ object FlinkNameJob {
       )
       .build();
 
+    val dataStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
+      .map(x => x.get("name").asText)
+      .keyBy(x => x)  // add a keyBy, so we can use mapWithState, which has to follow the keyBy
+                      // https://nightlies.apache.org/flink/flink-docs-master/docs/dev/datastream/fault-tolerance/state/
+                      // http://www.alternatestack.com/development/apache-flink-mapwithstate-on-keyedstream/
+      .mapWithState(
+        (in: String, count: Option[Int]) => {
+          count match {
+            case Some(num) => ((in, num + in.length), Some(num + in.length))
+            case _ => ((in, in.length), Some(in.length))
+          }
+        }
+      )
+      // .map(x => x._1)
+      .map(x => (x._1.toUpperCase, x._2))
+      .map(x => (x._1.split(" ").head, x._2))
+      .map(x => Map("firstname" -> x._1, "totalchars" -> x._2))
+      .map(x => Json(DefaultFormats).write(x))
+
+    dataStream.print()  // debug
     dataStream.sinkTo(sink)
 
     // execute program
